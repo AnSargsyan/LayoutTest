d33214e67c96ffcc8ba2d966e5d8f1c325cefc76
AnSargsyan <sargsyanani23@gmail.com>
2018-11-29 18:21:46 +0400

    mistakes

--- content/script/test.js
+++ content/script/test.js
@@ -1,51 +1,47 @@
 
- window.onload = function() {
+window.onload = function () {
 
-	var tab_item = $('.tab').children();
+    var tab_item = $('.tab').children();
 
-	for(let i = 0;  i < tab_item.length; i++) {
-	     $(`<li>`).text($(tab_item[i]).attr('name')).appendTo($('ul#nav_item'));
-	     var nav_item = $('li');	
+    for (let i = 0; i < tab_item.length; i++) {
+        $(`<li>`).text($(tab_item[i]).attr('name')).appendTo($('ul#nav_item'));
+        var nav_item = $('li');
+}
+        // var myArr = [];
 
-	var myArr = [];
+        for (let j = 0; j < nav_item.length; j++) {
+            nav_item[j].addEventListener('click', select);
+            // myArr.push(nav_item[j]);
+        
+        // var counter;
 
-	for(let j = 0;  j < nav_item.length; j++) {
-	    nav_item[j].addEventListener('click', select);
-	    myArr.push(nav_item[j]);
+        // function select(e) {
 
-	}
-		var counter;
+        //     $(nav_item[counter]).removeAttr('selected', 'selected')
+        //     $(tab_item[counter]).removeAttr('selected', '')
 
-		 function select(e){
- 
-		 	$(nav_item[counter]).removeAttr('selected', 'selected')
-		 	$(tab_item[counter]).removeAttr('selected', '')
-		 	
-			$(e.currentTarget).attr('selected', 'selected')
-			var index = myArr.indexOf(e.currentTarget);
-			counter = index;
-			$(tab_item[index]).attr('selected', '')
-	 	}	
-	}	
+        //     $(e.currentTarget).attr('selected', 'selected')
+        //     var index = myArr.indexOf(e.currentTarget);
+        //     counter = index;
+        //     $(tab_item[index]).attr('selected', '')
+        // }
+        	function select() {
+			for(let i = 0;  i < tab_item.length; i++) {
+
+		 		if(i == j) {
+					$(tab_item[i]).attr('selected', 'selected')
+					$(nav_item[j]).attr('selected', 'selected')
+				}
+				else {
+			       		$(tab_item[i]).removeAttr('selected', '')
+			       		$(nav_item[j]).removeAttr('selected', '')
+			        }
+		    }
+	 	}
+   }
 }
 // console.log($('[myId="home"]')[0]);
 
 
-	// function select() {
-		// 	for(let i = 0;  i < tab_item.length; i++) {
-		//  		if(i == j) {
-		// 			$(tab_item[i]).attr('selected', '')
-		// 		}
 
-		// 		if((i == j) && (tab_item[i].hasAttribute('selected'))) {
-
-		// 			$(nav_item[j]).css('background-image', 'linear-gradient(to bottom, rgba(255,255,255,.8)  0%,rgba(255,255,255,.3) 100%)')
-		// 			container = j;
-		// 		}	
-		// 		else {
-		// 	       		$(tab_item[i]).removeAttr('selected', '')
-		// 	       		$(nav_item[j]).css('background-image', '')
-		// 	        }
-		//     }
-	 // 	}
 
