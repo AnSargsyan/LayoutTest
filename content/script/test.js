a88a06b59e29b1414dc2da81345027eff3758cea
AnSargsyan <sargsyanani23@gmail.com>
2018-12-03 18:59:01 +0400

    change
    change

--- content/script/test.js
+++ content/script/test.js
@@ -6,39 +6,43 @@ window.onload = function () {
     for (let i = 0; i < tab_item.length; i++) {
         $(`<li>`).text($(tab_item[i]).attr('name')).appendTo($('ul#nav_item'));
         var nav_item = $('li');
+        $(nav_item[0]).attr('selected', 'selected')
 }
-        // var myArr = [];
+        var myArr = [];
 
         for (let j = 0; j < nav_item.length; j++) {
             nav_item[j].addEventListener('click', select);
-            // myArr.push(nav_item[j]);
+            myArr.push(nav_item[j]);
         
-        // var counter;
+        var counter;
 
-        // function select(e) {
+        function select(e) {
+            $('[selected = "selected"]').removeAttr('selected')
 
-        //     $(nav_item[counter]).removeAttr('selected', 'selected')
-        //     $(tab_item[counter]).removeAttr('selected', '')
+            $(nav_item[counter]).removeAttr('selected', 'selected')
+            $(tab_item[counter]).removeAttr('selected', 'selected')
 
-        //     $(e.currentTarget).attr('selected', 'selected')
-        //     var index = myArr.indexOf(e.currentTarget);
-        //     counter = index;
-        //     $(tab_item[index]).attr('selected', '')
-        // }
-        	function select() {
-			for(let i = 0;  i < tab_item.length; i++) {
+            $(e.currentTarget).attr('selected', 'selected')
+            var index = myArr.indexOf(e.currentTarget);
+            counter = index;
+            $(tab_item[index]).attr('selected', '')
+            
+        }
+        // function select() {
+		// 	for(let i = 0;  i < tab_item.length; i++) {
 
-		 		if(i == j) {
-					$(tab_item[i]).attr('selected', 'selected')
-					$(nav_item[j]).attr('selected', 'selected')
-				}
-				else {
-			       		$(tab_item[i]).removeAttr('selected', '')
-			       		$(nav_item[j]).removeAttr('selected', '')
-			        }
-		    }
-	 	}
+		//  		if(i == j) {
+		// 			$(tab_item[i]).attr('selected', 'selected')
+		// 			$(nav_item[j]).attr('selected', 'selected')
+		// 		}
+		// 		else {
+		// 	       		$(tab_item[i]).removeAttr('selected', '')
+		// 	       		$(nav_item[j]).removeAttr('selected', '')
+		// 	        }
+		//     }
+	 	// }
    }
+   
 }
 // console.log($('[myId="home"]')[0]);
 
